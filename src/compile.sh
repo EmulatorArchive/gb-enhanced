b63b3d12241f70eab15ef01d3fc4d86dcaa362f1
Daniel Baxter <shonumi@gmail.com>
2013-11-11 11:26:33 -0600

    g++ optimizations

--- src/compile.sh
+++ src/compile.sh
@@ -1,81 +1,81 @@
-if g++ -c config.cpp; then
+if g++ -c -O3 -funroll-loops config.cpp; then
 	echo -e "Compiling Config...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Config...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c mbc1.cpp; then
+if g++ -c -O3 -funroll-loops mbc1.cpp; then
 	echo -e "Compiling MBC1...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling MBC1...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c mbc2.cpp; then
+if g++ -c -O3 -funroll-loops mbc2.cpp; then
 	echo -e "Compiling MBC2...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling MBC2...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c mbc3.cpp; then
+if g++ -c -O3 -funroll-loops mbc3.cpp; then
 	echo -e "Compiling MBC3...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling MBC3...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c mbc5.cpp; then
+if g++ -c -O3 -funroll-loops mbc5.cpp; then
 	echo -e "Compiling MBC5...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling MBC5...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c mmu.cpp; then
+if g++ -c -O3 -funroll-loops mmu.cpp; then
 	echo -e "Compiling MMU...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling MMU...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c z80.cpp; then
+if g++ -c -O3 -funroll-loops z80.cpp; then
 	echo -e "Compiling Z80 CPU...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Z80 CPU...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c gamepad.cpp -lSDL; then
+if g++ -c -O3 -funroll-loops gamepad.cpp -lSDL; then
 	echo -e "Compiling Game Pad...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Game Pad...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c filter.cpp -lSDL; then
+if g++ -c -O3 -funroll-loops filter.cpp -lSDL; then
 	echo -e "Compiling Filters...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Filters...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c gpu.cpp -lSDL; then
+if g++ -c -O3 -funroll-loops gpu.cpp -lSDL; then
 	echo -e "Compiling GB GPU...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling GB GPU...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c hotkeys.cpp -lSDL; then
+if g++ -c -O3 -funroll-loops hotkeys.cpp -lSDL; then
 	echo -e "Compiling Hotkeys...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Hotkeys...			\E[31m[ERROR]\E[37m"
 	exit
 fi
 
-if g++ -c source.cpp -lSDL; then
+if g++ -c -O3 -funroll-loops source.cpp -lSDL; then
 	echo -e "Compiling Main...			\E[32m[DONE]\E[37m"
 else
 	echo -e "Compiling Main...			\E[31m[ERROR]\E[37m"
